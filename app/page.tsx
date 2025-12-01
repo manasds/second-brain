e9ab057ff6fc741be0b8f652f35c180f7c7aff41
md1 <mddevs21@gmail.com>
2025-12-01 19:54:50 +0530

    landing page created

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
     });
     console.log(cards);
     return (
-      <div className="min-w-0 h-full bg-neutral-200 pl-64 text-black">
+      <div className="min-w-0 h-full bg-neutral-200 pl-64 text-black ">
         <header className="flex justify-between p-3.5">
           <h1 className="font-medium ml-3.5">All notes</h1>
           <div className="flex justify-between gap-2.5">
@@ -45,7 +45,7 @@ export default async function Home() {
           /> */}
           {cards.map((card: Memory) => (
             <Card
-              id = {card.id}
+              id={card.id}
               title={card.title}
               content={card.content}
               link={card.link}
@@ -57,12 +57,17 @@ export default async function Home() {
     );
   }
   return (
-    <div className="min-w-0 h-full bg-neutral-200 pl-64 text-black">
-      <header className=" h-18 bg-neutral-600 flex justify-end px-2.5">
+    <div className="min-w-0 h-screen text-black flex flex-col absolute w-full">
+      <header className="flex justify-end p-3.5">
         <LoginButton />
       </header>
-      <div className="flex items-center justify-center">
-        <div className="text-2xl font-semibold">Please login first</div>
+      <div className="flex flex-col items-center justify-center pt-28 w-full gap-4 ">
+        <div className="font-semibold text-white text-5xl max-w-xl selection:text-black selection:bg-neutral-300 tracking-wide leading-16">
+          Attention span less than that of a Goldfish ?
+        </div>
+        <div className="text-white text-md flex justify-start max-w-xl w-full pl-1.5">
+          <h3>I wont fix it , duh . this is another note making app</h3>
+        </div>
       </div>
     </div>
   );
